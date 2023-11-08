a86389f2fe9b0eb7ea8c566c1f1cecd5e12a0cd7
maggle <lenny.mallet@free.fr>
2023-11-08 17:41:23 +0100

    changing PC

--- login.php
+++ login.php
@@ -5,4 +5,19 @@
     if (mysqli_connect_errno()){
         echo "erreur de connection: " . mysqli_connect_error();
     }
+
+
+//code pour le hashage
+$test = "test";
+$hash = password_hash($test, PASSWORD_BCRYPT);
+$hash2 = password_hash($test, PASSWORD_BCRYPT);
+
+
+echo $hash;
+if($hash == $hash2){
+    echo ("same");
+}
+else{
+    echo ("different");
+}
 ?>
--- navbar.php
+++ navbar.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mystyle.css">
+    <title>Navbar</title>
+</head>
+<body>
 <nav class="navbar">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -5,13 +14,15 @@
     </div>
     <ul class="nav navbar-nav">
         <li><a href='index.php'><img src='flowerlogo.jpg'></a></li>
-        <li class="active"><a href="#">Menu</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
+        <li><a href="products.php">Catalogue    </a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-user"></span>Se connecter</a></li>
     </ul>
   </div>
 </nav>
+</body>
+</html>
+
+
+
